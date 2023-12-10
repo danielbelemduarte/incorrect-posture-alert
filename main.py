15644ab9c871773e7dd7748addc33c04c242d147
Daniel Belém Duarte <danielbelemduarte@gmail.com>
2023-12-10 17:26:49 +0000

    added GPT analysis from the vision description

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import os
 from utils import read_prompt, play_alert, load_config_file, check_if_folder_exists, create_folder
 import time
+from openai import OpenAI
+import json
 
 # used to create folders and project structure if do not exist
 def project_setup_config():
@@ -48,7 +50,7 @@ def call_vision(image_path, api_key, vision_model, max_tokens):
     "Authorization": f"Bearer {api_key}"
     }
 
-    loaded_prompt =  read_prompt("prompt.txt")
+    loaded_vision_prompt =  read_prompt("prompt_analyze_photo_vision.txt")
 
     payload = {
     "model": vision_model,
@@ -58,7 +60,7 @@ def call_vision(image_path, api_key, vision_model, max_tokens):
         "content": [
             {
             "type": "text",
-            "text": f"{loaded_prompt}"
+            "text": f"{loaded_vision_prompt}"
             },
             {
             "type": "image_url",
@@ -74,17 +76,35 @@ def call_vision(image_path, api_key, vision_model, max_tokens):
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
     answer = response.json()['choices'][0]['message']['content']
 
-    #print(response.json())
-    print("Answer: " + str(answer))
     return str(answer)
 
+def analyze_posture_description(posture_analysis_description, completion_model, api_key):
+    client = OpenAI()
+    client.api_key = api_key
+
+    system_message = read_prompt("prompt_analyze_posture_description_system_message.txt")
+    user_message = read_prompt("prompt_analyze_posture_description_user_message.txt").replace("<<POSTURE_DESCRIPTION>>", posture_analysis_description)
+
+
+    response = client.chat.completions.create(
+    model=completion_model,
+    response_format={ "type": "json_object"},
+    messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message}
+    ]
+    )
+    print(response.choices[0].message.content)
+    return response.choices[0].message.content
 
 if __name__ == "__main__":
 
     project_setup_config()
 
     config_file = load_config_file()
-    MODEL_NAME = config_file['model_name']
+
+    VISION_MODEL_NAME = config_file['vision_model_name']
+    COMPLETION_MODEL_NAME = config_file['completion_model_name']
     MAX_TOKENS = config_file['max_tokens']
 
     # OPENAI API KEY MUST BE ADDED TO THE SYSTEM ENVIRONMENT VARIABLES WITH THE NAME ON THE CONFIG FILE 
@@ -95,21 +115,25 @@ if __name__ == "__main__":
         img_filename = save_frame_camera_key(0, 'data/temp', 'camera_capture')
         img_filename = img_filename.replace("/", "\\")
 
-        result = call_vision(image_path=img_filename, api_key=OPENAI_API_KEY, vision_model=MODEL_NAME, max_tokens=MAX_TOKENS)
+        posture_analysis_result = call_vision(image_path=img_filename, api_key=OPENAI_API_KEY, vision_model=VISION_MODEL_NAME, max_tokens=MAX_TOKENS)
 
-        print("Posture analysis:")
-        print(result)
+        print("Posture analysis from VISION:")
+        print(posture_analysis_result)
 
-        if "curved" in result.lower():
+        posture_result_payload = analyze_posture_description(posture_analysis_result, COMPLETION_MODEL_NAME, OPENAI_API_KEY)
+
+        print("Posture result from GPT:")
+        print(posture_result_payload)
+
+        parsed_json = json.loads(posture_result_payload)
+        posture = parsed_json["response"]
+
+        if "incorrect" in str(posture).lower():
             print("You are in an INCORRECT working posiiton!")
-
             play_alert()
-
-        elif "straight" in result.lower():
+        elif "correct" in str(posture).lower():
             print("You are in a CORRECT working posiiton!")
         else:
             print("Model could not confirm the position, check if images are capturing your body and posture for correct analysis!")
         # Wait for 5 minutes
         time.sleep(300)  # 300 seconds = 5 minutes
-
-
